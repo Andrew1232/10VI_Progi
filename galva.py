6f08c2ab8d83f3b26ba8d59b10d8ea9b7b8eb5aa
Andrew1232 <celms.egils@gmail.com>
2021-01-06 11:21:24 +0200

    1

--- galva.py
+++ galva.py
@@ -7,8 +7,19 @@ from funkc import tr_ga
 
 app = Flask(__name__)
 
+@app.route('/pogasall', methods=['GET'])
+def rut():
+  u=request.args.get('a',default=0,type=str)
+  u="ievadita vertiba: "+u
+  return render_template('sveikaPasaule.html', vards="ha",rezultats=u)
+
+
+@app.route('/pogas')
+def pogas():
+  return render_template('pogas.html',vards="poga")
+
 @app.route('/a',methods=['GET'])
-def root():
+def pogasall():
     aa=float(request.args.get('a',default='0',type=str))
     bb=float(request.args.get('b',default='0',type=str))
     cc=float(request.args.get('c',default='0',type=str))
